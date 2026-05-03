c26e8626df40de64e5383664f00ab5004d87101e
filatov-a <118124715+flamefks@users.noreply.github.com>
2026-05-04 00:42:51 +0300

    Fix setup nats

--- internal/shared/queue/nats/setup/stream.go
+++ internal/shared/queue/nats/setup/stream.go
@@ -8,12 +8,33 @@ import (
 )
 
 func EnsureStreams(ctx context.Context, js jetstream.JetStream) error {
-	_, err := js.CreateOrUpdateStream(ctx, jetstream.StreamConfig{
+	stream, err := js.CreateOrUpdateStream(ctx, jetstream.StreamConfig{
 		Name: sharedData.NatsStreamName,
 		Subjects: []string{
 			sharedData.JobsSubjectDeliver, sharedData.JobsSubjectFetcher,
 		},
 		Storage: jetstream.FileStorage,
 	})
+
+	_, err = stream.CreateOrUpdateConsumer(ctx, jetstream.ConsumerConfig{
+		Durable:       sharedData.FetcherGroup,
+		Name:          sharedData.FetcherGroup,
+		FilterSubject: sharedData.JobsSubjectFetcher,
+		AckPolicy:     jetstream.AckExplicitPolicy,
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = stream.CreateOrUpdateConsumer(ctx, jetstream.ConsumerConfig{
+		Durable:       sharedData.DeliverGroup,
+		Name:          sharedData.DeliverGroup,
+		FilterSubject: sharedData.JobsSubjectDeliver,
+		AckPolicy:     jetstream.AckExplicitPolicy,
+	})
+	if err != nil {
+		return err
+	}
+
 	return err
 }
